88d7d4ed0148caff5381310ca6f5f34ee5873457
Matin, Annie <Annie.Matin@ncr.com>
2019-04-29 14:51:31 -0400

    Cannot click on grid button if it's already been clicked

--- game.js
+++ game.js
@@ -3,85 +3,82 @@
     var result = document.getElementById("game-result");
     var reset = document.getElementById("reset");
 
-      // 2. Define the function that will respond to the event.
-      var turn = 0;
-      var player = "X";
-      var grid = [];
-      var gameOver = false;
+    var turn = 0;
+    var player = "X";
+    var grid = [];
+    var gameOver = false;
 
 
 
+    function checkWin(myNum, delta){
+      if(grid[myNum] != undefined){
+        if(grid[myNum] == grid[myNum-delta] && grid[myNum] == grid[myNum+delta] ){
+
+          result.textContent = "Player " +player + " wins!";
+          gameOver = true;
+
+        }
+
+
+      }
+     }
+
+      //Click tic tac toe buttons
       var onButtonClick = function() {
 
-        //button on grid is clicked
-        if(gameOver == false){
+        //get the index of the button clicked
+        var clickedInt = parseInt(this.id.substring(6, 7));
+
+        //player can only move if game is not over and a player has not clicked on that button
+        if(gameOver == false && grid[clickedInt] == undefined){
 
 
 
-            //X's turn
+            //figure out if current player is X or O
             if (turn % 2 == 0) {
               player = "X";
 
 
             }
-            //O's turn
             else {
               player = "O";
 
             }
             turn++;
+
+            //set button text and grid value to X or O
             this.textContent = player;
+            grid[clickedInt] = player;
 
 
 
 
+            checkWin(4,1);
+            checkWin(4,3);
+            checkWin(4,2);
+            checkWin(4,4);
+
+            checkWin(1,1);
+            checkWin(7,1);
+
+            checkWin(3,3);
+            checkWin(5,3);
 
 
-                    //add X or O to the button clicked
-                    var clickedInt = parseInt(this.id.substring(6, 7));
-                    grid[clickedInt] = player;
-
-                  function checkWin(myNum, delta){
-                    if(grid[myNum] != undefined){
-                      if(grid[myNum] == grid[myNum-delta] && grid[myNum] == grid[myNum+delta] ){
-
-                        result.textContent = "Player " +player + " wins!";
-                        gameOver = true;
-
-                      }
-
-
-                    }
-                   }
-                    checkWin(4,1);
-                    checkWin(4,3);
-                    checkWin(4,2);
-                    checkWin(4,4);
-
-                    checkWin(1,1);
-                    checkWin(7,1);
-
-                    checkWin(3,3);
-                    checkWin(5,3);
-
-
-                    //It's a tie if all grid spaces are filled but no wins
-                    if(turn == 9 && gameOver == false){
-                         gameOver = true;
-                         result.textContent = "It's a tie!";
-                    }
-
+            //It's a tie if all turns have been taken but no wins
+            if(turn == 9 && gameOver == false){
+                 gameOver = true;
+                 result.textContent = "It's a tie!";
             }
 
-
-
-
+        }
 
       }
+
+      //Click new game button
       var onResetButtonClick = function() {
         grid = [];
         turn = 0;
-        console.log(grid);
         result.textContent = "";
         gameOver = false;
         for (var i = 0; i < 9; i++) {
